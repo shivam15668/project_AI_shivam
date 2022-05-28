20170bf5ebfb30547a0c4d975800178f466a9f0c
shivamK <201b252@juetguna.in>
2022-05-28 10:51:17 +0530

    button

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -78,7 +78,40 @@ public class MainActivity extends AppCompatActivity {
         textView2.setText(operand1 + operator + operand2);
 
       // Your code here, to diplay correct and incorrect options on the buttons
-
+        int correctAnswer = -100 ;
+        if(operator.equals("+"))
+            correctAnswer = operand1+operand2 ;
+        else if(operator.equals("-"))
+            correctAnswer = operand1-operand2 ;
+        else if (operator.equals("*"))
+            correctAnswer = operand1*operand2 ;
+        else if(operator.equals("/"))
+            correctAnswer = operand1/operand2 ;
+        correctButton = random.nextInt(4);
+        if(correctButton == 0 ){
+            button1.setText(correctAnswer+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+2+"");
+        }
+        else if(correctButton == 1 ){
+            button1.setText(correctAnswer+3+"");
+            button2.setText(correctAnswer+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+2+"");
+        }
+        else if(correctButton == 2 ){
+            button1.setText(correctAnswer+3+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer+"");
+            button4.setText(correctAnswer+2+"");
+        }
+        else if(correctButton == 3 ){
+            button1.setText(correctAnswer+4+"");
+            button2.setText(correctAnswer+1+"");
+            button3.setText(correctAnswer-1+"");
+            button4.setText(correctAnswer+"");
+        }
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
             matchCounter=0;
